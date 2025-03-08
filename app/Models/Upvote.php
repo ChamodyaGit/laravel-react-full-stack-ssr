e83560a2d400177944fde81b03af68eb25fe2b8b
ChamodyaGit <backchamodyapunsara@gmail.com>
2025-03-08 11:45:17 +0530

    Implement upvote downvote

--- app/Models/Upvote.php
+++ app/Models/Upvote.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Upvote extends Model
 {
+    public $timestamps = false;
+
+    protected $fillable = ['feature_id', 'user_id', 'upvote'];
+
     public function feature()
     {
-        return $this->belongsTo(Feature::class, );
+        return $this->belongsTo(Feature::class,);
     }
 }
